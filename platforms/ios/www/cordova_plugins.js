194237c239eb936cbf2b7769f71842600782d49a
Andreas Joensen <andreas@joensen.eu>
2016-02-03 19:47:06 +0100

    added ios platform and updated index.html with test

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-inappbrowser": "1.2.2-dev",
+    "cordova-plugin-statusbar": "2.1.0",
+    "cordova-plugin-whitelist": "1.2.1"
+}
+// BOTTOM OF METADATA
+});
